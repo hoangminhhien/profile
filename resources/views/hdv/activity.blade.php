47b0856abbb135635a69a808fd87789405686699
hienhm1997 <hoangminhhien.th2006@gmail.com>
2020-09-18 10:57:06 +0700

    profile hdv

---
+++ resources/views/hdv/activity.blade.php
@@ -0,0 +1,73 @@
+<div id="activity" class="card class-border" style="border: none;">
+	<div class="card-body">
+		<strong style="font-size: 20px">
+			Nhật ký hoạt động
+		</strong>
+		<button type="button" class="btn btn-link" v-on:click="addActivity">
+			<i class="fa fa-plus" aria-hidden="true" style="cursor: pointer;" ></i>
+		</button>
+		<hr style="border: 1px solid black">
+		<div class="row" v-for="(activity, indexActivity) in listActivity">
+			<div class="col-md-3">
+				<input type="text" class="form-control dateActivity">
+			</div>
+			<div class="col-md-7">
+				<div class="form-group row">
+				    <label class="col-md-4 col-form-label font-weight-bold">Đơn vị tổ chức</label>
+				    <div class="col-md-8">
+				      <input type="text" class="form-control">
+				    </div>
+				</div>
+				<div class="form-group row">
+				    <label class="col-md-4 col-form-label font-weight-bold">Địa chỉ</label>
+				    <div class="col-md-8">
+				      <input type="text" class="form-control">
+				    </div>
+				</div>
+				<div class="form-group row">
+				    <label class="col-md-4 col-form-label font-weight-bold">Giới thiệu chương trình</label>
+				    <div class="col-md-8">
+				      <input type="text" name="" class="form-control">
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<button type="button" class="btn btn-danger" v-on:click="removeActivity(indexActivity)">
+					<i class="fa fa-minus" aria-hidden="true"></i> Xóa
+				</button>
+			</div>
+			<hr style="border: 1px dashed black; width: 100%">
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+	new Vue({
+        el: '#activity',
+        data() {
+            return {
+            	listActivity: []
+            }
+        },
+        methods:{
+        	addActivity() {
+        		console.log(1111111)
+        		this.listActivity.push({
+
+                        date: null,
+                        name_shool: null,
+                        specialized: null,
+                        classification: 0,
+                    })
+            },
+            removeActivity(indexActivity) {
+            	console.log(this.listActivity, indexActivity)
+                if (confirm('Bạn muốn xóa phần này?')) {
+                	console.log(indexActivity)
+
+                    this.listActivity.splice(indexActivity, 1)
+                }
+
+            },
+        }
+    })
+</script>
---
+++ resources/views/hdv/volunteer.blade.php
@@ -0,0 +1,73 @@
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<div id="volunteer" class="card class-border" style="border: none;">
+	<div class="card-body">
+		<strong style="font-size: 20px">
+			Chương trình tình nguyện tham gia
+		</strong>
+		<button type="button" class="btn btn-link" v-on:click="addEducation">
+			<i class="fa fa-plus" aria-hidden="true" style="cursor: pointer;" ></i>
+		</button>
+		<hr style="border: 1px solid black">
+		<div class="row" v-for="(eduction, indexEduction) in listEduction">
+			<div class="col-md-3">
+				<input type="text" class="form-control dateEducation">
+			</div>
+			<div class="col-md-7">
+				<div class="form-group row">
+				    <label class="col-md-4 col-form-label font-weight-bold">Tên hoạt động</label>
+				    <div class="col-md-8">
+				      <input type="text" class="form-control">
+				    </div>
+				</div>
+				<div class="form-group row">
+				    <label class="col-md-4 col-form-label font-weight-bold">Vị trí tham gia</label>
+				    <div class="col-md-8">
+				      <input type="text" class="form-control">
+				    </div>
+				</div>
+				<div class="form-group row">
+				    <label class="col-md-4 col-form-label font-weight-bold">Mô tả</label>
+				    <div class="col-md-8">
+				      <input type="text" class="form-control">
+				    </div>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<button type="button" class="btn btn-danger" v-on:click="removeEducation(indexEduction)">
+					<i class="fa fa-minus" aria-hidden="true"></i> Xóa
+				</button>
+			</div>
+			<hr style="border: 1px dashed black; width: 100%">
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+	new Vue({
+        el: '#volunteer',
+        data() {
+            return {
+            	listEduction: []
+            }
+        },
+        methods:{
+        	addEducation() {
+        		console.log(1111111)
+        		this.listEduction.push({
+                        date: null,
+                        name_shool: null,
+                        specialized: null,
+                        classification: 0,
+                    })
+            },
+            removeEducation(indexEduction) {
+            	console.log(this.listEduction, indexEduction)
+                if (confirm('Bạn muốn xóa phần này?')) {
+                	console.log(indexEduction)
+
+                    this.listEduction.splice(indexEduction, 1)
+                }
+
+            },
+        }
+    })
+</script>
